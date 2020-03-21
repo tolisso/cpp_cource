a77031bba82de2da739c21912cd41867e4b9b232
tolisso <malko01@yandex.ru>
2020-03-21 16:04:28 +0300

    few bugs was fixed

--- substring_finder_n_mul_m/main.cpp
+++ substring_finder_n_mul_m/main.cpp
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
+#include <cstring>
 
 using namespace std;
 
@@ -17,13 +17,13 @@ signed main(int argc, char* argv[]) {
         cout << "Illegal number of arguments" << endl;
         return 0;
     }
-    int32_t str_size = strlen(argv[1]);
-    char* str = argv[1];
+    int32_t str_size = (int32_t)strlen(argv[2]);
+    char* str = argv[2];
     bool mask[str_size];
     for (size_t i = 0; i < str_size; i++) {
-        mask[i] = 0;
+        mask[i] = false;
     }
-    int fd = open(argv[2], O_RDONLY | O_CLOEXEC);
+    int fd = open(argv[1], O_RDONLY | O_CLOEXEC);
     if (fd < 0)
     {
         perror("open failed");
@@ -44,10 +44,18 @@ signed main(int argc, char* argv[]) {
         if (bytes_read == 0) {
             break;
         }
-        for (int32_t i = 0; i < bytes_read; i++) {
+        for (size_t i = 0; i < bytes_read; i++) {
             char cur_ch = buffer[i];
+
+            for (int32_t j = str_size - 1; j >= 0; j--) {
+                if (j == 0 || mask[j - 1]) {
+                    mask[j] = (cur_ch == str[j]);
+                } else {
+                    mask[j] = false;
+                }
+            }
             if (cur_ch == str[str_size - 1] && mask[str_size - 1]) {
-                ssize_t bytes_written = write(STDOUT_FILENO, "true\n", 5);
+                ssize_t bytes_written = write(STDOUT_FILENO, "TRUE\n", 5);
                 if (bytes_written < 0)
                 {
                     perror("write failed");
@@ -57,16 +65,9 @@ signed main(int argc, char* argv[]) {
                 close(fd);
                 return EXIT_SUCCESS;
             }
-            for (int32_t j = str_size - 2; j >= 0; j--) {
-                if (j == 0 || mask[j]) {
-                    mask[j + 1] = (cur_ch == str[j]);
-                } else {
-                    mask[j + 1] = false;
-                }
-            }
         }
     }
-    ssize_t bytes_written = write(STDOUT_FILENO, "false\n", 6);
+    ssize_t bytes_written = write(STDOUT_FILENO, "FALSE\n", 6);
     if (bytes_written < 0)
     {
         perror("write failed");
