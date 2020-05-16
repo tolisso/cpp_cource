68931005c95a0a7efae7e84c1be49f5bb7cd5042
tolisso <malko01@yandex.ru>
2020-05-16 15:47:36 +0300

    hw4 added

---
+++ vector-demo-master/cmake-build-debug/CMakeFiles/vector_testing.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tolisso/Programming/cpp_course/vector-demo-master/gtest/gtest-all.cc" "/home/tolisso/Programming/cpp_course/vector-demo-master/cmake-build-debug/CMakeFiles/vector_testing.dir/gtest/gtest-all.cc.o"
+  "/home/tolisso/Programming/cpp_course/vector-demo-master/gtest/gtest_main.cc" "/home/tolisso/Programming/cpp_course/vector-demo-master/cmake-build-debug/CMakeFiles/vector_testing.dir/gtest/gtest_main.cc.o"
+  "/home/tolisso/Programming/cpp_course/vector-demo-master/main.cpp" "/home/tolisso/Programming/cpp_course/vector-demo-master/cmake-build-debug/CMakeFiles/vector_testing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vector-demo-master/cmake-build-debug/CMakeFiles/vector_testing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector_testing.dir/gtest/gtest-all.cc.o"
+  "CMakeFiles/vector_testing.dir/gtest/gtest_main.cc.o"
+  "CMakeFiles/vector_testing.dir/main.cpp.o"
+  "vector_testing"
+  "vector_testing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector_testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
